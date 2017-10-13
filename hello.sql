def1c3ec0517a0051de0792937f5a3a308e8ccca
helenzhang <xnbao@qq.com>
2017-10-13 17:42:14 +0800

    -1 line

--- hello.sql
+++ hello.sql
@@ -1,6 +1,8 @@
 ## add hub+5+7+9
 create table  hello
 
+-1 line
+-1 line
 
 ( id int(4) NOT NULL auto_increment, visit_date date,
 pv int(6),
@@ -11,5 +13,7 @@ sessions int(6)
 );
 the end
 -5-7
+-1 line
+-1 line
 -1 line
 -1 line
